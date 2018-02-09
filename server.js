17b15b43e5261cd0edb7826f98f1ab01876eaaed
tiger786007@gmail.com <tiger786007@gmail.com>
2018-02-09 21:25:18 +0530

    Initialize repo

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+
+var middleware=require('./middleware.js');
+
+// app.get('/',function(req , res){
+// 	res.send('hello express!');
+// });
+
+app.use(middleware.logger);
+//app.use(middleware.requireAuthentication);
+
+app.get('/about',middleware.requireAuthentication,function(req , res){
+	res.send('about us');
+});
+
+app.use(express.static(__dirname +'/public'));
+
+app.listen(port,function(){
+	console.log('Express server started :'+ port);
+});

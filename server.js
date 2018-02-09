b2f0a6bb02a78b6cf2a36659d747d264540c7fae
tiger786007@gmail.com <tiger786007@gmail.com>
2018-02-09 21:41:11 +0530

    Updated language

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(middleware.logger);
 //app.use(middleware.requireAuthentication);
 
 app.get('/about',middleware.requireAuthentication,function(req , res){
-	res.send('about us');
+	res.send('about us!');
 });
 
 app.use(express.static(__dirname +'/public'));
